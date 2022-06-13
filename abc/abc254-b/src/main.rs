aa192e3c420e4ec01d1b8260e4b6476105b0e262
hossy <hossy@hossy.info>
2022-06-13 23:54:26 +0900

    added abc254 a-e

---
+++ abc/abc254-b/src/main.rs
@@ -0,0 +1,17 @@
+// -*- coding:utf-8-unix -*-
+
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+    }
+    let mut a = [[0; 30]; 30];
+    for i in 0..n {
+        for j in 0..=i {
+            a[i][j] = if j == 0 || j == i { 1 } else { a[i - 1][j - 1] + a[i - 1][j] };
+            print!("{}{}", if j == 0 { "" } else { " " }, a[i][j]);
+        }
+        println!();
+    }
+}
---
+++ abc/abc254-b/tests/sample_inputs.rs
@@ -0,0 +1,41 @@
+use cli_test_dir::*;
+
+const BIN: &'static str = "./main";
+
+#[test]
+fn sample1() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(r#"3"#)
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), r#"1
+1 1
+1 2 1
+"#);
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample2() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(r#"10
+        "#)
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), r#"1
+1 1
+1 2 1
+1 3 3 1
+1 4 6 4 1
+1 5 10 10 5 1
+1 6 15 20 15 6 1
+1 7 21 35 35 21 7 1
+1 8 28 56 70 56 28 8 1
+1 9 36 84 126 126 84 36 9 1
+"#);
+    assert!(output.stderr_str().is_empty());
+}
