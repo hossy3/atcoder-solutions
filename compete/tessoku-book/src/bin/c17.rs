a01461f9c2eb4885e8f7d471688a09b928ca0bcc
hossy <hossy@hossy.info>
2023-02-14 22:59:23 +0900

    updated tessoku-book-c17

--- compete/tessoku-book/src/bin/c17.rs
+++ compete/tessoku-book/src/bin/c17.rs
@@ -9,11 +9,6 @@ fn main() {
 
     let mut v0 = VecDeque::new();
     let mut v1 = VecDeque::new();
-    let f = |v0: &mut VecDeque<_>, v1: &mut VecDeque<_>| {
-        if v0.len() + 1 == v1.len() {
-            v0.push_back(v1.pop_front().unwrap());
-        }
-    };
 
     for _ in 0..q {
         input! {
@@ -24,24 +19,24 @@ fn main() {
                 input! {
                     x: String,
                 }
-                f(&mut v0, &mut v1);
                 v1.push_back(x);
             }
             'B' => {
                 input! {
                     x: String,
                 }
-                f(&mut v0, &mut v1);
                 v1.push_front(x);
             }
             'C' => {
-                f(&mut v0, &mut v1);
                 v0.pop_front();
             }
             _ => {
-                let x = v0.front().unwrap_or(&v1[0]);
+                let x = v0.front().unwrap();
                 println!("{}", x);
             }
+        }
+        if v0.len() + 1 == v1.len() {
+            v0.push_back(v1.pop_front().unwrap());
         }
     }
 }
