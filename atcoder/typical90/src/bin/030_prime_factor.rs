a8c9bb702217445453d4cdd20dda430b17693e6a
hossy <hossy@hossy.info>
2024-01-23 00:24:32 +0900

    Add 030 and 031

---
+++ atcoder/typical90/src/bin/030_prime_factor.rs
@@ -0,0 +1,26 @@
+use proconio::input;
+
+fn build_prime_factor_nums(n: usize) -> Vec<usize> {
+    let mut primes = vec![0; n + 1];
+    for i in 2..=n {
+        if primes[i] > 0 {
+            continue;
+        }
+        let m = n / i;
+        for j in 1..=m {
+            primes[i * j] += 1;
+        }
+    }
+    primes
+}
+
+fn main() {
+    input! {
+        n: usize,
+        k: usize,
+    }
+
+    let prime_factor_nums = build_prime_factor_nums(n);
+    let count = prime_factor_nums.iter().filter(|&&x| x >= k).count();
+    println!("{count}");
+}
---
+++ atcoder/typical90/src/bin/031_grundy.rs
@@ -0,0 +1,32 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        ws: [usize; n],
+        bs: [usize; n],
+    }
+
+    const N: usize = 50;
+    let b_max = N * (N + 1) / 2 + N + 1;
+    let mut grundy = vec![vec![0; b_max]; N + 1];
+
+    for w in 0..=N {
+        for b in 0..b_max {
+            let mut mex = vec![0usize; b_max];
+            if w >= 1 && b + w < b_max {
+                mex[grundy[w - 1][b + w]] = 1;
+            }
+            if b >= 2 {
+                for k in 1..=(b / 2) {
+                    mex[grundy[w][b - k]] = 1;
+                }
+            }
+            grundy[w][b] = mex.iter().position(|&x| x == 0).unwrap();
+        }
+    }
+
+    let xor = std::iter::zip(ws, bs).fold(0, |acc, (w, b)| acc ^ grundy[w][b]);
+    let yes = xor != 0;
+    println!("{}", if yes { "First" } else { "Second" });
+}
