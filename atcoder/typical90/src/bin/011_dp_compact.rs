e12b080fd207b26f35ae335c14db6f9cba9f1516
hossy <hossy@hossy.info>
2024-02-04 22:29:09 +0900

    Update typical90-011-compact

--- atcoder/typical90/src/bin/011_dp_compact.rs
+++ atcoder/typical90/src/bin/011_dp_compact.rs
@@ -5,23 +5,22 @@ fn main() {
         n: usize,
         mut dcs: [(usize, usize, usize); n],
     }
-    dcs.sort_by_key(|&(d, c, _)| d as i64 - c as i64);
+    dcs.sort();
 
     const D: usize = 5000;
-    let mut v = vec![0usize; D + 1];
-    v[0] = 0;
+    let mut dp = vec![vec![0usize; D + 1]; n + 1];
+    dp[0][0] = 0;
 
-    for &(d, c, s) in &dcs {
-        let prev = v.clone();
-        for i in 0..=D {
-            v[i] = v[i].max(prev[i]);
-            let j = i + c;
-            if j <= d {
-                v[j] = v[j].max(prev[i] + s);
+    for (i, &(d, c, s)) in dcs.iter().enumerate() {
+        for j in 0..=D {
+            dp[i + 1][j] = dp[i + 1][j].max(dp[i][j]);
+            let j0 = j + c;
+            if j0 <= d {
+                dp[i + 1][j0] = dp[i + 1][j0].max(dp[i][j] + s);
             }
         }
     }
 
-    let result = v.iter().max().unwrap();
+    let result = dp[n].iter().max().unwrap();
     println!("{result}");
 }
