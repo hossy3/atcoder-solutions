d3c428f3e365a88bc13deaf84ad3a977f259f74b
hossy <hossy@hossy.info>
2024-02-17 20:46:39 +0900

    Update typical90-030

--- atcoder/typical90/src/bin/030_prime_factor.rs
+++ atcoder/typical90/src/bin/030_prime_factor.rs
@@ -1,6 +1,6 @@
 use proconio::input;
 
-fn build_prime_factor_nums(n: usize) -> Vec<usize> {
+fn build_prime_factor_nums(n: usize) -> Vec<u8> {
     let mut primes = vec![0; n + 1];
     for i in 2..=n {
         if primes[i] > 0 {
@@ -17,7 +17,7 @@ fn build_prime_factor_nums(n: usize) -> Vec<usize> {
 fn main() {
     input! {
         n: usize,
-        k: usize,
+        k: u8,
     }
 
     let prime_factor_nums = build_prime_factor_nums(n);
