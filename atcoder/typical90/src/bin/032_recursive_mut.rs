e0e198e98d513c04cd9ee92e4adc2d570331ddab
hossy <hossy@hossy.info>
2024-02-21 23:38:44 +0900

    Add typical90-032-mut

---
+++ atcoder/typical90/src/bin/032_recursive_mut.rs
@@ -0,0 +1,46 @@
+use proconio::{input, marker::Usize1};
+use std::collections::HashSet;
+
+fn f(a: &[Vec<usize>], s: &HashSet<(usize, usize)>, v: &mut Vec<usize>, score: usize) -> usize {
+    let n = a.len();
+    let mut min_score = usize::MAX;
+    for i in 0..n {
+        if v.iter().any(|&x| x == i) {
+            continue;
+        }
+        let j = v.len();
+        if j > 0 && s.contains(&(v[j - 1], i)) {
+            continue;
+        }
+        let score = score + a[i][j];
+        if j + 1 == n {
+            min_score = min_score.min(score);
+        } else {
+            v.push(i);
+            min_score = min_score.min(f(a, s, v, score));
+            v.pop();
+        }
+    }
+    min_score
+}
+
+fn main() {
+    input! {
+        n: usize,
+        a: [[usize; n]; n],
+        m: usize,
+        xy: [(Usize1, Usize1); m],
+    }
+    let mut s = HashSet::<(usize, usize)>::new();
+    for (x, y) in xy {
+        s.insert((x, y));
+        s.insert((y, x));
+    }
+    let mut v = vec![];
+    let min_score = f(&a, &s, &mut v, 0);
+    if min_score == usize::MAX {
+        println!("-1");
+    } else {
+        println!("{min_score}");
+    }
+}
