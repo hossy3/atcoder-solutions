743d5b3242b3bec8707f2a534ada76473ea6c72b
hossy <hossy@hossy.info>
2024-07-03 00:37:48 +0900

    Add typical90-044-vec-tle

--- atcoder/typical90/src/bin/044_vec_tle.rs
+++ atcoder/typical90/src/bin/044_vec_tle.rs
@@ -1,5 +1,4 @@
 use proconio::input;
-use std::collections::VecDeque;
 
 fn main() {
     input! {
@@ -8,15 +7,15 @@ fn main() {
         a: [usize; n],
         txy: [(u8, usize, usize); q],
     }
-    let mut v = VecDeque::from(a);
+    let mut v = a.clone();
     for (t, x, y) in txy {
         match t {
             1 => {
                 v.swap(x - 1, y - 1);
             }
             2 => {
-                let x = v.pop_back().unwrap();
-                v.push_front(x);
+                let x = v.pop().unwrap();
+                v.insert(0, x);
             }
             3 => {
                 println!("{}", v[x - 1]);
